dbdad5e4d80a9c495d7a52f742ba0b0d9bf738f9
Felipe <jfelipesmarques@gmail.com>
2026-01-09 15:18:44 -0300

    Added reset function

--- js/script.js
+++ js/script.js
@@ -4,13 +4,17 @@ const selectClass = document.querySelector("#class_select");
 
 const submittButton = document.querySelector("#submit_button");
 
+const form = document.querySelector("form")
+
 function charBuilder(event) {
   event.preventDefault();
 
   const formName = inputName.value;
   const classPicked = selectClass.value;
 
-  alert("Hero: " + formName + " | Class: " + classPicked + "has been created");
+  alert("Hero: " + formName + " | Class: " + classPicked + " has been created");
+
+  form.reset()
 }
 
 submittButton.addEventListener('click', charBuilder);
