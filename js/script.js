8d1227321aacb3bb9dae41f4efb0befc2cc78ce7
Felipe <jfelipesmarques@gmail.com>
2026-01-09 16:05:37 -0300

    finished

--- js/script.js
+++ js/script.js
@@ -6,13 +6,22 @@ const submittButton = document.querySelector("#submit_button");
 
 const form = document.querySelector("form")
 
+const outputDiv = document.querySelector("#char-output")
+
 function charBuilder(event) {
   event.preventDefault();
 
   const formName = inputName.value;
   const classPicked = selectClass.value;
 
-  alert("Hero: " + formName + " | Class: " + classPicked + " has been created");
+  outputDiv.innerHTML = `
+  <div class="game-resume">
+    <h3>Your Hero has been created!</h3>
+    <p>Name: ${formName}</p>
+    <p>Class: ${classPicked}</p>
+    <p>Status: Level 1 - Ready to start!</p>
+  </div>
+`;
 
   form.reset()
 }
